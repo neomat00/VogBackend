3efd7a9f83b086d1cc10db8783a4b9522cd70668
rahul <rahulramesh1995@gmail.com>
2021-03-22 17:53:57 -0600

    question 2

---
+++ VogCodeChallenge.API/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Models
+{
+    public class Department
+    {
+        public string Name { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ VogCodeChallenge.API/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace VogCodeChallenge.API.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public string Address { get; set; }
+        public Department Department { get; set; }
+    }
+}
