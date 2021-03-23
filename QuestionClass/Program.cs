a45fce86fa9ab7440baa4f465d45864a03caddb9
rahul <rahulramesh1995@gmail.com>
2021-03-23 03:51:22 -0600

    Question7

---
+++ QuestionClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuestionClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuestionClass.QuestionA;
+        }
+    }
+}
---
+++ QuestionClass/QuestionClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionClass
+{
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<String>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+    }
+
+    public static void TestQuestion()
+    {
+        object NamesList = null;
+        using (var Names = NamesList.GetEnumerator())
+        {
+            while (Names.MoveNext())
+            {
+                Console.WriteLine(Names.Current);
+            }
+        }
+    }
+
+
+}
