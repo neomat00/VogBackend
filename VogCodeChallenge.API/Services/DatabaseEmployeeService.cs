3d75fd36207fe742d828aa25a763c54b578c3720
rahul <rahulramesh1995@gmail.com>
2021-03-23 01:16:03 -0600

    question 6

---
+++ VogCodeChallenge.API/Services/DatabaseEmployeeService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VogCodeChallenge.API.Models;
+
+// This would be the replication of database implementation
+
+namespace VogCodeChallenge.API.Services
+{
+    public class DatabaseEmployeeService : IEmployeeService
+    {
+        private IList<Employee> _employees;
+
+        private IList<Department> _departments;
+        public DatabaseEmployeeService()
+        {
+            _departments = new List<Department>
+            {
+                new Department { Name = "Hr", Address = "TEST1"},
+                new Department { Name = "Dev", Address = "TEST2"},
+                new Department { Name = "Pr", Address = "TEST3"}
+            };
+            _employees = new List<Employee>
+            {
+                new Employee { FirstName = "Test1FN", LastName = "Test1LN", Address = "Test1A", Department = _departments[0]},
+                new Employee { FirstName = "Test2FN", LastName = "Test2LN", Address = "Test2A", Department= _departments[1] },
+                new Employee { FirstName = "Test3FN", LastName = "Test3LN", Address = "Test3A", Department = _departments[2]},
+            };
+
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _employees;
+        }
+
+        public IList<Employee> ListAll()
+        {
+            return _employees;
+        }
+    }
+}
